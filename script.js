e5e033e5f59193df6c21ae70ff2b4629d19ee363
mahadghani <126356929+mahadghani@users.noreply.github.com>
2024-06-24 16:15:17 -0700

    Update script.js

--- script.js
+++ script.js
@@ -6,3 +6,13 @@ document.addEventListener("DOMContentLoaded", function () {
     document.querySelector(".container").style.display = "flex";
   }, 1500);
 });
+
+document.getElementById("themeStyle_1").disabled = true;
+document.getElementById("themeStyle_2").disabled = true;
+
+const randomNumber = Math.floor(Math.random() * 3) + 1;
+if (randomNumber == 1) {
+  document.getElementById("themeStyle_1").disabled = false;
+} else if (randomNumber == 2) {
+  document.getElementById("themeStyle_2").disabled = false;
+}
